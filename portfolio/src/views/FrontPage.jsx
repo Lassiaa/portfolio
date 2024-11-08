f0f178a2c45dd0d7a5f046cb20d0c2b39bd180ae
Lassiaa <91322139+Lassiaa@users.noreply.github.com>
2024-11-08 16:23:39 +0200

    kuva testi

--- portfolio/src/views/FrontPage.jsx
+++ portfolio/src/views/FrontPage.jsx
@@ -112,21 +112,7 @@ const FrontPage = () => {
     width: scrolled,
   };
 
-  const images = [
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194190654000070676/IMG_2964.PNG?ex=65af73b6&is=659cfeb6&hm=d51997d5e3e90f63ed8fa8a54537e693216e22fa837be27ef765834df4c43887&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185564694511676/Silvia-S15.png?ex=65af6ef9&is=659cf9f9&hm=5c2296f374b5a5d4ce105de38af4d4e4dc55d739387cb68ccd8ca4df78ce5854&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185477897588826/Guy-With-Lantern.png?ex=65af6ee4&is=659cf9e4&hm=153e7c4af84556230b107839164652e6fd75f99bbf9290267568e3e7e50220b4&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185527331655690/Kossi-painting.png?ex=65af6ef0&is=659cf9f0&hm=9147fe730ce68a3b17fefa03c8df5509f44499354c7f548c443cccc14d2dff5e&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185529357520936/Guy-With-Pitchfork.png?ex=65af6ef1&is=659cf9f1&hm=740e46c004d1237a217480c746aaf3284bdcb8085ef39aa0b6acfb3f4912e5db&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185475813015602/Concept-One.png?ex=65af6ee4&is=659cf9e4&hm=786210a9315330788f6f376d4cc78bd8bfa9855bbe4d9eb54c25882ebc0afc39&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185565709541477/Wizard.png?ex=65af6ef9&is=659cf9f9&hm=c8ca4403ddd93a08aa42f8ecde7e661aaafb2a48e24cfa917d3a8b257fa8b691&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185528304730122/r34_v2.png?ex=65af6ef0&is=659cf9f0&hm=07261bc61cfaee85557501dbccc6b6192ae7c229c56d7148e028278689143484&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185447988019240/240girl.jpg?ex=65af6edd&is=659cf9dd&hm=20f53221faa1a118e7e973d985715713e3eca794464096e97ed110974f49cb72&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194190940345212938/Untitled_Artwork.png?ex=65af73fb&is=659cfefb&hm=dbc5167ab52319d28b858093522eb0deb4f02cc14d2c70b5f79bdf3f286a9462&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185448461971476/Baby-Yoda.png?ex=65af6edd&is=659cf9dd&hm=50cf8251ccf22c2b35f2527946bc1206d781701f823064c964f4d0d00ab7bc5d&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185476576399430/Donut.png?ex=65af6ee4&is=659cf9e4&hm=c872400e610fc2556c557e7a1811e472e0a56ba470633c9f990e26b851368b78&",
-    "https://cdn.discordapp.com/attachments/571697001826222096/1194185449212756080/city-night-time.png?ex=65af6edd&is=659cf9dd&hm=eea60bae50b9d31f09cef2242ea83730914ef3d685e36917e36f3140cb3cc935&",
-  ];
+  const images = ["./assets/img/240girl.jpg"];
 
   const emailToCopy = "lassi.antero.aaltonen@gmail.com";
 
@@ -199,8 +185,7 @@ const FrontPage = () => {
       <div
         className={style.bannerImage}
         style={{
-          backgroundImage:
-            "url('https://static.vecteezy.com/system/resources/previews/021/583/370/non_2x/aesthetic-background-illustrations-of-clouds-stars-and-sky-with-purple-gradations-suitable-for-wallpapers-presentation-backgrounds-and-various-other-design-needs-free-vector.jpg')",
+          backgroundImage: "url('./assets/img/lassijaporsche.jpeg')",
         }}
       >
         <div className={style.bannerContent}>
@@ -213,8 +198,7 @@ const FrontPage = () => {
               <div
                 className={style.lassiImg}
                 style={{
-                  backgroundImage:
-                    "url('https://cdn.discordapp.com/attachments/571697001826222096/1194003037602189412/cachedImage.PNG?ex=65aec4fb&is=659c4ffb&hm=06f7a2edd998f1eafc5328f0734416b037f3371d202b7c2ad3f75f0f6b1676cd&')",
+                  backgroundImage: "url('')",
                 }}
               >
                 <div className={style.lassiDarken}></div>
@@ -264,8 +248,7 @@ const FrontPage = () => {
               <div
                 className={style.infoImg}
                 style={{
-                  backgroundImage:
-                    "url('https://cdn.discordapp.com/attachments/1104847004686897245/1194272959670534294/46e204e0-6cd0-4637-a557-a08012d02279.JPG?ex=65afc05e&is=659d4b5e&hm=40e6d03126de3a381d2169f4649dc372221ca55b231fd767d5319f8a139f7c90&')",
+                  backgroundImage: "url('')",
                 }}
               >
                 <div className={style.imgDarken}></div>
@@ -312,8 +295,7 @@ const FrontPage = () => {
               <div
                 className={style.teamImg}
                 style={{
-                  backgroundImage:
-                    "url('https://cdn.discordapp.com/attachments/1104847004686897245/1194181504570507314/image.png?ex=65af6b31&is=659cf631&hm=33331e3ee973fd772cd8f4eb05604f76d4c22823c755b6c72564544acc9d863c&')",
+                  backgroundImage: "url('')",
                 }}
               >
                 <div className={style.imgDarken}></div>
@@ -344,8 +326,7 @@ const FrontPage = () => {
               <div
                 className={style.teamImg}
                 style={{
-                  backgroundImage:
-                    "url('https://cdn.discordapp.com/attachments/1104847004686897245/1194181879885213766/image.png?ex=65af6b8a&is=659cf68a&hm=ff882eda679bdb0091670b2aa101a479777faee8d1d85b31ce03b57b8b29b523&')",
+                  backgroundImage: "url('')",
                 }}
               >
                 <div className={style.imgDarken}></div>
@@ -374,8 +355,7 @@ const FrontPage = () => {
               <div
                 className={style.teamImg}
                 style={{
-                  backgroundImage:
-                    "url('https://cdn.discordapp.com/attachments/1104847004686897245/1194182284916576317/Bac-Buddy.png?ex=65af6beb&is=659cf6eb&hm=e2f892b4686b5f698979362c42a7b51abe597a234abc5875f70901a3ca5d92f6&')",
+                  backgroundImage: "url('')",
                 }}
               >
                 <div className={style.imgDarken}></div>
